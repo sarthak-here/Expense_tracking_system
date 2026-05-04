a0744eed1c879f9e24286e49b63a276e1881e9e8
sarthak-here <sarthak909999@gmail.com>
2026-05-05 05:20:55 +0530

    completed the frontend and api calling tests

--- Frontend/analyticss.py
+++ Frontend/analyticss.py
@@ -1,4 +1,33 @@
 import streamlit as st
 from datetime import datetime
 import requests
+import pandas as pd
 
+API_URL = "http://localhost:8000"
+
+def analytics():
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("Start Date", datetime(2024, 8, 1))
+    with col2:
+        end_date = st.date_input("End Date", datetime(2024, 8, 5))
+    if st.button("Get Analytics"):
+        payload = {
+            "start_date": start_date.strftime("%Y-%m-%d"),
+            "end_date": end_date.strftime("%Y-%m-%d")
+        }
+        response = requests.post(f"{API_URL}/analytics/", json=payload)
+        if response.status_code == 200:
+            response = response.json()
+        
+        data = {
+            "Category": list(response.keys()),
+            "Total": [response[category]['total'] for category in response],
+            "Percentage": [response[category]['percentage'] for category in response]
+        }
+
+        df = pd.DataFrame(data)
+        df_sorted = df.sort_values(by="Percentage", ascending=False)
+        st.table(df_sorted)
+        st.title("Expense Breakdown")
+        st.bar_chart(df_sorted.set_index("Category")["Percentage"])
--- Frontend/app.py
+++ Frontend/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from datetime import datetime
 import requests
+from addorupdate import add_or_update_expense
+from analyticss import analytics
 
 API_URL = "http://localhost:8000"
 
@@ -9,52 +11,7 @@ st.title("Expense Tracker")
 tab1, tab2 = st.tabs(["Add Expense", "Analytics"])
 
 with tab1:
-    selected_date = st.date_input("Enter Date", datetime(2024, 8, 1), label_visibility="collapsed")
-    response = requests.get(f"{API_URL}/expenses/{selected_date}")
-    if response.status_code ==200:
-        existing_expenses = response.json()
-        st.write(existing_expenses)
-    else:
-        st.write("failed to retrive expenses")
-        existing_expenses = []
-    with st.form(key="expense_form"):
-        
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.subheader(f"Amount")
-        with col2:
-            st.subheader(f"Category")
-        with col3:
-            st.subheader(f"Notes")
-        categories = ["Rent", "Shopping", "Food", "Transport", "Entertainment", "Utilities", "Other"]
-        expenses = []
-        for i in range(5):
-            
-            if i < len(existing_expenses):
-                amount = existing_expenses[i]["amount"]
-                category = existing_expenses[i]["category"]
-                notes = existing_expenses[i]["notes"]
-            else:
-                amount = 0.0
-                category = "Food"
-                notes = ""
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                amount_input =st.number_input(label="Amount", min_value=0.0, step=1.0, value=amount, key=f"amount_{i}_{selected_date}", label_visibility="collapsed")
-            with col2:
-                category_input =st.selectbox(label="Category", options=categories, key=f"category_{i}_{selected_date}", index=categories.index(category), label_visibility="collapsed")
-            with col3:
-                notes_input =st.text_input(label="Notes", value=notes, key=f"notes_{i}_{selected_date}", label_visibility="collapsed")
-            expenses.append({
-                'amount': amount_input,
-                'category': category_input,
-                'notes': notes_input
-            })
-        submit_button = st.form_submit_button()
-        if submit_button:
-            filtered_expenses = [{**expense, 'expense_date': str(selected_date)} for expense in expenses if expense['amount'] > 0]
-            response = requests.post(f"{API_URL}/expenses/{selected_date}", json=filtered_expenses)
-            if response.status_code == 200:
-                st.success("Expenses saved successfully!")
-            else:
-                st.error("Failed to save expenses.")
+    add_or_update_expense()
+    
+with tab2:
+    analytics()
